86e6795e0d2aec0788f90e3749177ab3c70723a2
grey <strainenator@gmail.com>
2015-09-05 19:29:50 +0200

    updates: - add bootstrap 3 - models.py - requirements.txt

--- apps/mainp/models.py
+++ apps/mainp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class MainPage(models.Model):
+    title = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=100)
+    bdate = models.DateField(blank=False, null=False)
+    cemail = models.EmailField(max_length=254)
+    cjabber = models.CharField(max_length=100)
+    cskype = models.CharField(max_length=100)
+    bio = models.TextField()
+    ocontacts = models.TextField()
