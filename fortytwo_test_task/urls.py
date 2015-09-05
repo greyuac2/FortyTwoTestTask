d8bf1ea899482174f8e96dbf1d34249aaed4b69c
grey <strainenator@gmail.com>
2015-09-05 20:30:25 +0200

    ticket:1 added urls.py

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -8,6 +8,5 @@ urlpatterns = patterns(
     # Examples:
     # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
